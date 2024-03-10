3a6715535a0b21992632dd5643d21911553c9ab7
lzzzzz14 <1257041806@qq.com>
2024-03-10 23:11:24 +0800

    3.10景点介绍

--- WQW毕设/bert/爬取景点介绍.py
+++ WQW毕设/bert/爬取景点介绍.py
@@ -1,24 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import time
 
-url = 'https://you.ctrip.com/sight/shenzhen26/2778.html'
+file_path = '../attractions.xlsx'
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
-}
+df = pd.read_excel(file_path)
 
-response = requests.get(url, headers=headers)
+site_names = df['景点名称'].tolist()
+urls = df['网址'].tolist()
 
-if response.status_code == 200:
-    html_doc = response.text
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    paragraphs = soup.find_all('p', class_='inset-p')
-    extracted_text = [p.get_text() for p in paragraphs]
-    print(extracted_text)
+i = 0
 
-file_path = '1'
+for i in range(0, len(urls)):
 
-with open(file_path, 'w', encoding='utf-8') as file:
-    for line in extracted_text:
-        file.write(line + "\n")
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/122.0.0.0 Safari/537.36'
+    }
+
+    txt_path = f'../景点评价/{site_names[i]}.txt'
+
+    response = requests.get(urls[i], headers=headers)
+
+    if response.status_code == 200:
+        html_doc = response.text
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        module_title = soup.find('div', class_='moduleTitle')
+        module_content = module_title.find_next_sibling('div', class_='moduleContent')
+        extracted_text = [p.get_text(strip=True) for p in module_content.find_all('p')]
+
+    with open(txt_path, 'w', encoding='utf-8') as file:
+        for line in extracted_text:
+            file.write(line + "\n")
+
+    i += 1
+    print(f'{i}：{site_names[i]}')
+
+    time.sleep(3)
 
