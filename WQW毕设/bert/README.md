fd9fb83d449f36dd6aff9fcce192daea91326031
lzzzzz14 <1257041806@qq.com>
2024-03-12 10:01:39 +0800

    3.12_lz

---
+++ WQW毕设/bert/README.md
@@ -0,0 +1,6 @@
+1. 景点名字+景点介绍
+![img_1.png](img_1.png)
+2. 景点介绍
+![img.png](img.png)
+3. 景点介绍（3类）
+![img_2.png](img_2.png)
--- WQW毕设/bert/bert.py
+++ WQW毕设/bert/bert.py
@@ -6,8 +6,16 @@ import numpy as np
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
+# 检查是否有CUDA支持的GPU
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("There is a GPU available, using the GPU...")
+else:
+    device = torch.device("cpu")
+    print("No GPU available, using the CPU instead...")
+
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertModel.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased').to(device)
 
 folder_path = "../景点评价"
 
@@ -15,33 +23,63 @@ file_names = os.listdir(folder_path)
 
 texts = []
 
+save_name = []
+
 for file_name in file_names:
     file_path = os.path.join(folder_path, file_name)
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
+        sight_name = file_name[:-4]
+        save_name.append(sight_name)
+        text = sight_name + ": " + text
         texts.append(text)
 
 # 提取特征
 features = []
 for text in texts:
-    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
     outputs = model(**inputs)
-    features.append(outputs.last_hidden_state[:, 0, :].detach().numpy())  # 取CLS标记的输出作为特征
+    if torch.cuda.is_available():
+        features.append(outputs.last_hidden_state[:, 0, :].cpu().numpy())
+    else:
+        features.append(outputs.last_hidden_state[:, 0, :].detach().numpy())  # 取CLS标记的输出作为特征
 
 features = np.vstack(features)  # 将特征列表转换为numpy数组
 
 # 聚类
-kmeans = KMeans(n_clusters=4)  # 假设我们想要分成4个类别
+kmeans = KMeans(n_clusters=3)  # 假设我们想要分成4个类别
 kmeans.fit(features)
 labels = kmeans.labels_  # 获取每个文本的类别标签
 
 print(labels)  # 打印每个文本的类别
 
+label_0 = []
+label_1 = []
+label_2 = []
+label_3 = []
+
+for i in range(0, len(labels)):
+    # labels=0
+    if labels[i] == 0:
+        label_0.append(save_name[i])
+    elif labels[i] == 1:
+        label_1.append(save_name[i])
+    elif labels[i] == 2:
+        label_2.append(save_name[i])
+    elif labels[i] == 3:
+        label_3.append(save_name[i])
+
+print('label_0: ', label_0)
+print('label_1: ', label_1)
+print('label_2: ', label_2)
+print('label_3: ', label_3)
+
 tsne = TSNE(n_components=2, random_state=0)
 reduced_features = tsne.fit_transform(features)
 
 # 根据聚类标签对降维后的特征进行颜色编码
-colors = ['r', 'b', 'g', 'c']  # 假设我们有4个聚类，分别用红色、蓝色、绿色和青色表示
+# colors = ['r', 'b', 'g', 'c']  # 假设我们有4个聚类，分别用红色、蓝色、绿色和青色表示
+colors = ['r', 'b', 'g']
 for i in range(len(colors)):  # 这里更改循环的次数来匹配聚类的数目
     x = reduced_features[labels == i, 0]
     y = reduced_features[labels == i, 1]
